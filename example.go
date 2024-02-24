a11f8fede74e32056ecec3f0262b118bfbeb9c17
aadv1k <aadv1k@outlook.com>
2024-02-24 18:45:00 +0530

    BUG: Highlights how neither `FindVertex` nor `FindVertexDFS` functions work correctly

--- example.go
+++ example.go
@@ -9,18 +9,32 @@ func main() {
     var g giraffe.Graph
 
     v0 := &giraffe.Vertex{Index: 0, Siblings: []*giraffe.Vertex{}}
-    v1 := &giraffe.Vertex{Index: 1, Siblings: []*giraffe.Vertex{v0}}
+    v1 := &giraffe.Vertex{Index: 1, Siblings: []*giraffe.Vertex{}}
+    v2 := &giraffe.Vertex{Index: 2, Siblings: []*giraffe.Vertex{}}
+    v3 := &giraffe.Vertex{Index: 3, Siblings: []*giraffe.Vertex{}}
+    v4 := &giraffe.Vertex{Index: 4, Siblings: []*giraffe.Vertex{}}
 
     g.AddVertex(v0)
     g.AddVertex(v1)
+    g.AddVertex(v2)
+    g.AddVertex(v3)
+    g.AddVertex(v4)
 
-    fmt.Println("Vertices:")
-    for _, v := range g.Vertices {
-        fmt.Println("\tVertex:", v.Index)
-    }
+    g.AddEdge(&giraffe.Edge{Start: v0, End: v1})
+    g.AddEdge(&giraffe.Edge{Start: v1, End: v2})
+    g.AddEdge(&giraffe.Edge{Start: v2, End: v3})
+    g.AddEdge(&giraffe.Edge{Start: v3, End: v4})
+    g.AddEdge(&giraffe.Edge{Start: v4, End: v0})
+    g.AddEdge(&giraffe.Edge{Start: v0, End: v2})
+    g.AddEdge(&giraffe.Edge{Start: v1, End: v3})
+    g.AddEdge(&giraffe.Edge{Start: v2, End: v4})
+    g.AddEdge(&giraffe.Edge{Start: v3, End: v0})
+    g.AddEdge(&giraffe.Edge{Start: v4, End: v1})
 
-    fmt.Println("Edges:")
-    for _, e := range g.Edges {
-        fmt.Printf("\tEdge: %d -> %d\n", e.Start.Index, e.End.Index)
-    }
+		found, visited := g.FindVertex(5)
+		fmt.Printf("BFS: [%d] ", found.Index)
+		for _, v := range visited {
+			fmt.Printf("%d, ", v.Index)
+		}
+		fmt.Println()
 }
