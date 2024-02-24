751025211ecc1a387ce19dcfc6c16ba06f700827
aadv1k <aadv1k@outlook.com>
2024-02-24 16:35:06 +0530

    Rewrite the entire base logic for graph representation
    Instead of representing the entire Graph as just a list of vertices and their, and their sibling, we
    now have the notion of an edge, and a stray vertex. This is a more accurate graph representation

--- giraffe/giraffe.go
+++ giraffe/giraffe.go
@@ -1,92 +1,64 @@
 package giraffe
 
-import (
-    "fmt"
-)
-
-type Node struct {
-    index          int
-    total_siblings int
-    siblings       []*Node
+type Vertex struct {
+	Index    int
+	Siblings []*Vertex
 }
 
-type Graph []*Node
-
-// Find a particular node
-func FindNode(g Graph, index int) *Node {
-    for _, node := range g {
-        if node.index == index {
-            return node
-        }
-    }
-    return nil
+type Edge struct {
+	Start *Vertex
+	End   *Vertex
 }
 
-// Create node with or without children
-func CreateNode(g *Graph, index int, siblings []int) *Node {
-    if foundNode := FindNode(*g, index); foundNode != nil {
-        fmt.Printf("A node already exists with index %d\n", foundNode.index)
-        return foundNode
-    }
-
-    node := &Node{index: index, siblings: []*Node{}}
-
-    for _, siblingIndex := range siblings {
-        if foundNode := FindNode(*g, siblingIndex); foundNode == nil {
-            fmt.Printf("Sibling node %d doesn't exist!\n", siblingIndex)
-            continue
-        }
-        sibling := FindNode(*g, siblingIndex)
-        node.siblings = append(node.siblings, sibling)
-    }
-
-    *g = append(*g, node)
-    return node
+type Graph struct {
+	Vertices []*Vertex
+	Edges    []*Edge
 }
 
-func VisualizeNode(n *Node) {
-    fmt.Printf("%d\n", n.index)
-    for _, sibling := range n.siblings {
-        VisualizeNodeSiblings(sibling, "", true)
-    }
+func (g *Graph) AddEdge(e *Edge) {
+	g.Edges = append(g.Edges, e)
 }
 
-func VisualizeNodeSiblings(n *Node, prefix string, directChild bool) {
-    if !directChild {
-        fmt.Printf("%s└───%d\n", prefix, n.index)
-    }  else {
-        fmt.Printf("└───%d\n", n.index)
+func (g *Graph) AddVertex(v *Vertex) {
+	g.Vertices = append(g.Vertices, v)
 
-    }
-
-
-    for _, sibling := range n.siblings {
-        if directChild {
-            VisualizeNodeSiblings(sibling, prefix+"    ", false)
-            continue
-        }
-        VisualizeNodeSiblings(sibling, prefix+"|   ", false)
-    }
+	for _, sibling := range v.Siblings {
+		g.Edges = append(g.Edges, &Edge{v, sibling})
+	}
 }
 
-func GetCentralityDegree(g *Graph) []float32 {
-    var ret []float32
+// Do a Breadth-First Search to find the vertex
+func (g *Graph) FindVertex(index int) *Vertex {
+	var queue []*Vertex
+	var visited []*Vertex
 
-    for i, node := range *g {
-        current := node.index
-        var degree int
+	queue = append(queue, g.Vertices[0])
 
-        for j := i; j < len(*g); j++ {
-            for _, x := range (*g)[j].siblings {
-                if x.index == current {
-                    degree++
-                }
-            }
-        }
+	for len(queue) != 0 {
+		currentNode := queue[0]
+		queue = queue[1:]
 
-        centrality := float32(degree) / float32(len(*g) - 1)
-        ret = append(ret, centrality)
-    }
+		nodeIsVisited := false
 
-    return ret
+		for _, visitedVtx := range visited {
+			if visitedVtx.Index == currentNode.Index {
+				nodeIsVisited = true
+				break
+			}
+		}
+
+		if nodeIsVisited {
+			break
+		}
+
+		if currentNode.Index == index {
+			return currentNode
+		}
+
+		for _, sibling := range currentNode.Siblings {
+			queue = append(queue, sibling)
+		}
+	}
+
+	return nil
 }
